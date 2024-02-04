36bdddde8a0a92b4a15ae71c507eebd9f48fbb14
samarth <samarthkulkarni@icloud.com>
2024-02-04 21:47:07 +0530

    creation optimisation

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -48,13 +48,13 @@ class binary_tree{
         void create_recursive(node*root);
         void preorder(node*root);
         void preorder();
-        void nonrec_preorder();
+        void non_recursive_preorder();
         void inorder(node*root);
         void inorder();
-        void nonrec_inorder();
+        void non_recursive_inorder();
         void postorder(node*root);
         void postorder();
-        void nonrec_postorder();
+        void non_recursive_postorder();
 };
 
 void binary_tree::build(){
@@ -66,157 +66,59 @@ void binary_tree::build(){
     create(root);
 }
 
-/*void binary_tree :: build_recursive()
-{
-    root = new node();
-    cout<<"Enter data for Root node \n";
-    cin>>root->data;
-    cout<<endl;
-    root->left = root->right = NULL;
-    create_recursive(root);
-}
-*/
 void binary_tree::create(node *root){
     char choice;
     cout << "enter q to stop giving input" << endl;
     do {
-        node *temp = root;
-        node *ptr = new node();
-        string path;
+        cout << "add data?" << endl;
+        cin >> choice;
+        if (choice != 'q'){
+            node *temp = root;
+            node *ptr = new node();
+            string path;
 
-        cout << "path: ";
-        cin>>path;
-        cout<<"\ndata: ";
-        cin>>ptr->data;
-        cout<<endl;
+            cout << "path: ";
+            cin>>path;
+            cout<<"\ndata: ";
+            cin>>ptr->data;
+            cout<<endl;
 
-        bool invalidPath = false;
+            bool invalidPath = false;
 
-        for(int i = 0; i < path.length(); i++) {
-            char c = path[i];
+            for(int i = 0; i < path.length(); i++) {
+                char c = path[i];
 
-            if (c == '0') {
-                if (i == path.length()-1){
-                    if(temp == NULL){
-                        cerr << "invalid path" << endl;
-                        invalidPath = true;
-                        break;
-                    } else {
-                        temp->left = ptr;
-                    }
-                } else {
-                    if(temp == NULL){
-                        cerr << "invalid path" << endl;
-                        invalidPath = true;
-                        break;
+                if(temp == NULL){
+                    cerr << "invalid path" << endl;
+                    invalidPath = true;
+                    break;
+                }
+
+                if (c == '0') {
+                    if (i == path.length()-1){
+                        temp->left = ptr;   
                     } else {
                         temp = temp->left;
                     }
-                }
-            } else if (c == '1') {
-                if (i == path.length()-1){
-                    if(temp == NULL){
-                        cerr << "invalid path" << endl;
-                        invalidPath = true;
-                        break;
-                    } else {
-                        temp->right = ptr;
-                    }
-                } else {
-                    if(temp == NULL){
-                        cerr << "invalid path" << endl;
-                        invalidPath = true;
-                        break;
+                } else if (c == '1') {
+                    if (i == path.length()-1){
+                        temp->right = ptr;   
                     } else {
                         temp = temp->right;
                     }
+                } else {
+                    cerr << "invalid path" << endl;
+                    invalidPath = true;
+                    break;
                 }
-            } else {
-                cerr << "invalid path" << endl;
-                invalidPath = true;
-                break;
+                if(invalidPath) break;
             }
-            if(invalidPath) break;
         }
-
-        cout << "add data?" << endl;
-        cin >> choice;
-
-
-        /*int flag=0;
-        char ch;
-        node *temp = root;
-        node *ptr = new node();
-        cout<<"Enter the  data\n";
-        cin>> ptr->data;
-        cout<<endl;
-        ptr->left = ptr->right = NULL;
-        while(flag == 0){
-            cout<<"Where do you want to enter the data: Right(R) or Left(L)"<<" "<<temp->data<<"\n";
-            cin>>ch;
-            cout<<endl;
-            if(ch == 'L')
-            {
-                if(temp->left == NULL)
-                {
-                    temp->left=ptr;
-                    flag = 1;
-                }
-                else
-                {
-                    temp=temp->left;
-                    flag=0;
-                }
-            }
-            if(ch == 'R')
-            {
-                if(temp->right == NULL)
-                {
-                    temp->right=ptr;
-                    flag = 1;
-                }
-                else
-                {
-                    temp=temp->right;
-                    flag=0;
-                }
-            }
-        }*/
+        
     }while(choice != 'q');
 }
-/*
-void binary_tree ::create_recursive(node *root)
-{
-         node *ptr;
-    char ch;
-    cout<<"Do you want to attach node on Left? Yes(Y) or No(N)\n";
-    cin>>ch;
-    cout<<endl;
-    if(ch == 'Y')
-    {
-        ptr = new node();
-        cout<<"Enter data of node\n";
-        cin>>ptr->data;
-        ptr->left = ptr->right = NULL;
-        root->left = ptr;
-        create_recursive(ptr);
-    }
-    cout<<"Do you want to attach node on Right? Yes(Y) or No(N)\n";
-    cin>>ch;
-    cout<<endl;
-    if(ch == 'Y')
-    {
-        ptr = new node();
-        cout<<"Enter data of node\n";
-        cin>>ptr->data;
-        ptr->left = ptr->right = NULL;
-        root->right = ptr;
-        create_recursive(ptr);
-    }
-}
-*/
-void binary_tree::preorder(node *root)
-{
+
+void binary_tree::preorder(node *root){
     if(root!=NULL)
     {
         cout<<root->data;
@@ -225,17 +127,15 @@ void binary_tree::preorder(node *root)
     }
 }
 
-void binary_tree::preorder()
-{
-    cout<<"\n preorder traversal :";
+void binary_tree::preorder() {
+    cout<<"\npreorder traversal :";
     preorder(root);
 }
 
-void binary_tree::nonrec_preorder()
-{
+void binary_tree::non_recursive_preorder() {
     stack s;
     node *temp = root;
-     cout<<"\n nonrec_preorder traversal :";
+     cout<<"\nnon_recursive_preorder traversal :";
     while(1)
     {
         while(temp!=NULL)
@@ -253,8 +153,7 @@ void binary_tree::nonrec_preorder()
    
 }
 
-void binary_tree::inorder(node*root)
-{
+void binary_tree::inorder(node*root) {
     if(root!=NULL)
     {
         inorder(root->left);
@@ -263,17 +162,15 @@ void binary_tree::inorder(node*root)
     }
 }
 
-void binary_tree::inorder()
-{
-    cout<<"\n inorder traversal :";
+void binary_tree::inorder() {
+    cout<<"\ninorder traversal :";
     inorder(root);
 }
 
-void binary_tree::nonrec_inorder()
-{
+void binary_tree::non_recursive_inorder() {
     stack s;
     node *temp = root;
-    cout<<"\n nonrec_inorder traversal :";
+    cout<<"\nnon_recursive_inorder traversal :";
     while(1)
     {
         while(temp!=NULL)
@@ -291,8 +188,7 @@ void binary_tree::nonrec_inorder()
     
 }
 
-void binary_tree::postorder(node*root)
-{
+void binary_tree::postorder(node*root) {
     if(root!=NULL)
     {
         postorder(root->left);
@@ -301,20 +197,17 @@ void binary_tree::postorder(node*root)
     }
 }
 
-void binary_tree::postorder()
-{
-    cout<<"\n postorder traversal :";
+void binary_tree::postorder() {
+    cout<<"\npostorder traversal :";
     postorder(root);
 }
 
-void binary_tree::nonrec_postorder()
-{
+void binary_tree::non_recursive_postorder() {
     stack s1, s2;
     node *temp = root;
     s1.push(temp);
-    cout<<"\n nonrec_postorder traversal :";
-    while(!s1.empty())
-    {
+    cout<<"\nnon_recursive_postorder traversal:";
+    while(!s1.empty()) {
         temp = s1.pop();
         s2.push(temp);
         if(temp->left!=NULL)
@@ -322,8 +215,7 @@ void binary_tree::nonrec_postorder()
         if(temp->right!=NULL)
             s1.push(temp->right);
     }
-    while(!s2.empty())
-    {
+    while(!s2.empty()) {
         temp = s2.pop();
         cout<<temp->data;
     }
@@ -331,16 +223,14 @@ void binary_tree::nonrec_postorder()
     
 }
 
-int main()
-{
+int main() {
 	binary_tree b;
 	b.build();
-	//b.build_recursive();
 	b.preorder();
-	b.nonrec_preorder();
+	b.non_recursive_preorder();
 	b.inorder();
-	b.nonrec_inorder();
+	b.non_recursive_inorder();
 	b.postorder();
-	b.nonrec_postorder();
+	b.non_recursive_postorder();
 	return 0;
 }
